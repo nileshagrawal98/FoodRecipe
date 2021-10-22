ea77d652ca1e4fb394ebabc4a2b93c99a9625196
Nilesh Agrawal <nilesh_fw12_098@masai.school>
2021-10-22 09:48:49 +0530

    Changed Favicon

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,6 @@
 function navbar() {
     return `<nav class="nav-container">
-    <img src="https://img.icons8.com/ios-glyphs/30/000000/cutlery.png" />
+    <img src="https://img.icons8.com/ios/30/000000/cookbook.png" />
     <a href="index.html" class="nav-option">Home</a>
     <a href="recipeOfDay.html" class="nav-option">Recipe of Day</a>
     <a href="latest.html" class="nav-option">Latest Recipe</a>
@@ -10,7 +10,7 @@ function navbar() {
 function setPageIcon(){
     let icon = document.createElement("link");
     icon.rel = "icon";
-    icon.href = "https://img.icons8.com/ios-glyphs/30/000000/cutlery.png";
+    icon.href = "https://img.icons8.com/ios-filled/30/ffffff/cookbook.png";
     icon.type = "image/icon type";
     document.head.append(icon);
 }
