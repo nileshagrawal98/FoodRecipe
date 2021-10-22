275c47560d6c95a5b8d09f53d56378d07d859634
Nilesh Agrawal <nilesh_fw12_098@masai.school>
2021-10-22 09:41:21 +0530

    Added favicon and title

--- components/navbar.js
+++ components/navbar.js
@@ -1,9 +1,18 @@
 function navbar() {
     return `<nav class="nav-container">
+    <img src="https://img.icons8.com/ios-glyphs/30/000000/cutlery.png" />
     <a href="index.html" class="nav-option">Home</a>
     <a href="recipeOfDay.html" class="nav-option">Recipe of Day</a>
     <a href="latest.html" class="nav-option">Latest Recipe</a>
 </nav>`
 }
 
-export default navbar;
+function setPageIcon(){
+    let icon = document.createElement("link");
+    icon.rel = "icon";
+    icon.href = "https://img.icons8.com/ios-glyphs/30/000000/cutlery.png";
+    icon.type = "image/icon type";
+    document.head.append(icon);
+}
+
+export {navbar, setPageIcon};
